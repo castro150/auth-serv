0f5051a43013514a166dce5570efe521f43308d2
castro150 <rafaelcarneiroget@hotmail.com>
2016-03-29 22:47:23 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/auth/serv/AuthServApplication.java
@@ -0,0 +1,12 @@
+package br.com.auth.serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServApplication.class, args);
+	}
+}
